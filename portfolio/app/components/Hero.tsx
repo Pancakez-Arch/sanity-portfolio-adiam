e1e0b5469cbe2506f459d43c6d8f6cbf2fa9ae60
Ad-#1-am <thedarkgamer256@gmail.com>
2024-12-05 10:16:32 +0100

    Finished: Projects page, done

---
+++ portfolio/app/components/Hero.tsx
@@ -0,0 +1,7 @@
+export function Hero() {
+    return (
+        <div>
+            <h1>what's up</h1>
+        </div>
+    )
+}
--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -3,16 +3,14 @@ import { ProjectsCard } from "../lib/interface";
 import { client } from "../lib/sanity";
 
 async function getData() {
-  const query = `*[_type == 'project'] {
-    title,
-        _id,
-        link,
-        description,
-        tags,
-        "imageUrl": image.asset->url
-
-
-        }`;
+  const query = `*[_type == 'projects'] {
+  title,
+  _id,
+  link,
+  description,
+  tags,
+  "imageUrl": image.asset->url
+}`;
 
   const data = await client.fetch(query);
 
@@ -36,7 +34,28 @@ export default async function ProjectsPage() {
             target="_blank"
           >
             <div className="aspect-w-16 aspect-h-12 overflow-hidden rounded-2xl relative">
-              <Image src={item.imageUrl} alt="Image Description"/>
+              <Image
+                src={item.imageUrl}
+                alt="Image Description"
+                fill
+                className="object-cover group-hover:scale-105 transition-transform duartion-500 ease-in-out rounded-2xl"
+              />
+            </div>
+            <div className="mt-4">
+                <h2 className="font-medium text-lg hover:underline">{item.title}</h2>
+                <p className="mt-1 text-muted-foreground line-clamp-3">{item.description}</p>
+                <div className="mt-3 flex flex-wrap gap-2">
+                    
+                    {item.tags.map((tag) => (
+                      <span
+                        key={tag}
+                        className="bg-muted inline-flex items-center rounded-full px-3 py-1 text-sm font-semibold text-muted-foreground"
+                      >
+                        {tag}
+                      </span>
+                    ))}
+
+                </div>
             </div>
           </a>
         ))}
